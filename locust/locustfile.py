fbbe05b5b2631d0183b2731133c6f537fbcb8ef5
helverinio <h.wiesner@uniandes.edu.co>
2024-09-07 13:25:07 -0500

    add locust to docker

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -3,13 +3,14 @@ import uuid
 
 class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
+    host = "http://localhost" 
+
+    #@task
+    #def ping(self):
+    #   self.client.get("/clientes/ping")
 
     @task
-    def index(self):
-        self.client.get("/clientes/ping")
-
-    @task
-    def index(self):
+    def queryuser(self):
         new_guid = str(uuid.uuid4())
-        self.client.get("/clientes/{newguid}")
+        self.client.get(f"/clientes/{new_guid}")
     

f185f5d56f8f32617e3784eeaecf090610b0fc2d
orlandoarnedo <oarnedo@progi.com>
2024-09-07 14:44:23 -0500

    monitor progress

--- monitor/src/models/model.py
+++ monitor/src/models/model.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 
 class Monitor(db.Model):
     id = db.Column(db.String(128), primary_key=True)
-    request = db.Column(db.String(32), nullable=False, unique=True)  # Client request or endpoint
+    request = db.Column(db.String(32), nullable=False, unique=False)  # Client request or endpoint
     status = db.Column(db.String(32), nullable=False)  # Health status (healthy, unhealthy, unreachable)
     response_code = db.Column(db.Integer, nullable=True)  # HTTP status code
     error_message = db.Column(db.String(256), nullable=True)  # Error message if any
@@ -31,23 +31,17 @@ class MonitorSchema(SQLAlchemyAutoSchema):
     updateAt = fields.DateTime()
 
 def save_health_status(health_status):
-    monitor_entry = Monitor.query.filter_by(request='client-service-ping').first()
+    new_uuid = str(uuid4())
 
-    if monitor_entry:
-        monitor_entry.status = health_status.get("status")
-        monitor_entry.response_code = health_status.get("code", None)
-        monitor_entry.error_message = health_status.get("error", None)
-        monitor_entry.updateAt = datetime.utcnow()  # Update the timestamp
-    else:
-        monitor_entry = Monitor(
-            id=str(uuid4()),  # Generate a unique ID
-            request='client-service-ping', 
-            status=health_status.get("status"),
-            response_code=health_status.get("code", None),
-            error_message=health_status.get("error", None),
-            createdAt=datetime.utcnow(),
-            updateAt=datetime.utcnow()
-        )
-        db.session.add(monitor_entry)
+    monitor_entry = Monitor(
+        id=new_uuid,
+        request='client-service-ping',
+        status=health_status.get("status"),
+        response_code=health_status.get("code", None),
+        error_message=health_status.get("error", None),
+        createdAt=datetime.utcnow(),
+        updateAt=datetime.utcnow()
+    )
 
+    db.session.add(monitor_entry)
     db.session.commit()
